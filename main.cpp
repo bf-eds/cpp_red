10b1e42877eda2e68e8c67c28f36a313065a1a4d
bf-eds <d.eroshenkov@dreamkas.ru>
2018-12-22 13:06:18 +0300

    week :one: ass. :eight:

--- main.cpp
+++ main.cpp
@@ -6,130 +6,28 @@
 using namespace std;
 
 
-class Human
+template<typename It>
+struct IteratorRange
 {
-public:
-    Human(const string &name, const string &type) : Name(name), Type(type)
+    It first, last;
+
+    IteratorRange(It begin, It end) : first(begin), last(end)
     {
-    }
-
-    void Log() const
-    {
-        cout << Type << ": ";
-    }
-
-    virtual void Walk(const string &destination) const = 0;
-public:
-    string Name;
-    string Type;
-};
-
-
-class Student : public Human
-{
-public:
-
-    Student(const string &name, const string &favouriteSong) : Human(name, "Student"), FavouriteSong(favouriteSong)
-    {
-    }
-
-    void Learn() const
-    {
-        Log();
-        cout << Name << " learns" << endl;
-    }
-
-    void SingSong() const
-    {
-        Log();
-        cout << Name << " sings a song: " << FavouriteSong << endl;
-    }
-
-    void Walk(const string &destination) const
-    {
-        Log();
-        cout << Name << " walks to: " << destination << endl;
-        SingSong();
-    }
-
-private:
-    string FavouriteSong;
-};
-
-
-class Teacher : public Human
-{
-public:
-
-    Teacher(const string &name, const string &subject) : Human(name, "Teacher"), Subject(subject)
-    {
-    }
-
-    void Walk(const string &destination) const
-    {
-        Log();
-        cout << Name << " walks to: " << destination << endl;
-    }
-
-    void Teach() const
-    {
-        Log();
-        cout << Name << " teaches: " << Subject << endl;
-    }
-
-private:
-    string Subject;
-};
-
-
-class Policeman : public Human
-{
-public:
-    Policeman(const string &name) : Human(name, "Policeman")
-    {
-    }
-
-    void Walk(const string &destination) const
-    {
-        Log();
-        cout << Name << " walks to: " << destination << endl;
-    }
-
-    void Check(const Human &t) const
-    {
-        Log();
-        cout << Name << " checks " << t.Type << ". " << t.Type << "'s name is: " << t.Name << endl;
     }
 };
 
-
-void VisitPlaces(Human &t, const vector<string> &places)
+template<typename T>
+auto MakeRange(T &container)
 {
-    for (auto p : places)
-    {
-        t.Walk(p);
-    }
+    return IteratorRange(container.begin(), container.end());
 }
 
 int main()
 {
-    Teacher t("Jim", "Math");
-    Student s("Ann", "We will rock you");
-    Policeman p("Bob");
-
-    VisitPlaces(t, {"Moscow", "London"});
-    p.Check(s);
-    VisitPlaces(s, {"Moscow", "London"});
+    IteratorRange vs_rng(10, 23);
+    const string s = "Hello, world!";
+    IteratorRange<string::const_iterator> rng = MakeRange(s);
+    string ss = "Hello, world!";
+    auto rng1 = MakeRange(ss);
     return 0;
 }
-
-
-/*
-Teacher: Jim walks to: Moscow
-Teacher: Jim walks to: London
-Policeman: Bob checks Student. Student's name is: Ann
-Student: Ann walks to: Moscow
-Student: Ann sings a song: We will rock you
-Student: Ann walks to: London
-Student: Ann sings a song: We will rock you
- */
